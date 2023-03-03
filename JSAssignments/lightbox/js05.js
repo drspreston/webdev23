c4f69c8ac8573fa9460c8cf7c1bacdfbbd2c4e6f
Silvia Preston <spreston@prcc.edu>
2023-03-03 09:36:24 -0600

    Added overlay image to the gallery

--- JSAssignments/lightbox/js05.js
+++ JSAssignments/lightbox/js05.js
@@ -79,7 +79,38 @@ function createLightbox(){
       let image = document.createElement("img");
       image.src = imgFiles[i];
       image.alt = imgCaptions[i];
+      image.onclick = createOverlay;
       lbImages.appendChild(image);
+   }
+
+   //Overlay function
+   function createOverlay(){
+      let overlay = document.createElement("div");
+      overlay.id = "lbOverlay";
+
+      //Add the figure box to the overlay
+      let figureBox = document.createElement("figure");
+      overlay.appendChild(figureBox);
+
+      //Add the image to the figure box
+      let overlayImage = this.cloneNode("true");
+      figureBox.appendChild(overlayImage);
+
+      //Add the caption to the figure box
+      let overlayCaption = document.createElement("figcaption");
+      overlayCaption.textContent = this.alt;
+      figureBox.appendChild(overlayCaption);
+
+      //Add a close button to the overlay
+      let closeBox = document.createElement("div");
+      closeBox.id = "lbOverlayClose";
+      closeBox.innerHTML = "&times;";
+      closeBox.onclick = function(){
+         document.body.removeChild(overlay);
+      }
+      overlay.appendChild(closeBox);
+
+      document.body.appendChild(overlay);
    }
 } //end createlightbox function
 
